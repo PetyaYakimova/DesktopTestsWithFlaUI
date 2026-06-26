ed1ebce947a544c5dea05cea3e5442a9f4fd8d29
Petya Yakimova <petya_yakimova@abv.bg>
2026-06-26 08:23:55 +0300

    Add test base logic

--- NotepadDesktopTests/Base/TestBase.cs
+++ NotepadDesktopTests/Base/TestBase.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlaUI.Core;
+using FlaUI.UIA3;
+using NUnit.Framework;
 
-namespace NotepadDesktopTests.Base
+namespace NotepadTests.Base;
+
+public class TestBase
 {
-    internal class TestBase
+    protected Application App;
+    protected UIA3Automation Automation;
+
+    [SetUp]
+    public void Setup()
     {
+        App = Application.Launch("notepad.exe");
+        Automation = new UIA3Automation();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Automation?.Dispose();
+
+        if (App != null && !App.HasExited)
+        {
+            App.Close();
+        }
     }
 }
