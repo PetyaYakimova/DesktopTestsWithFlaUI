b72d1b0e1815f599cc3028a298a4c8426752961b
Petya Yakimova <petya_yakimova@abv.bg>
2026-06-28 19:35:46 +0300

    Add notepad page

--- NotepadDesktopTests/Pages/NotepadPage.cs
+++ NotepadDesktopTests/Pages/NotepadPage.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Definitions;
+using FlaUI.UIA3;
 
-namespace NotepadDesktopTests.Pages
+namespace NotepadTests.Pages;
+
+public class NotepadPage
 {
-    internal class NotepadPage
+    private readonly Window _window;
+    private readonly UIA3Automation _automation;
+
+    public NotepadPage(Window window, UIA3Automation automation)
     {
+        _window = window;
+        _automation = automation;
+    }
+
+    public TextBox Editor =>
+        _window.FindFirstDescendant(
+            cf => cf.ByControlType(ControlType.Document))
+        ?.AsTextBox();
+
+    public void EnterText(string text)
+    {
+        Editor.Enter(text);
+    }
+
+    public string GetText()
+    {
+        return Editor.Text;
     }
 }
