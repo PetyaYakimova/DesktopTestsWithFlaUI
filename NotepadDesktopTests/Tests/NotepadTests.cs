763b39704d922ebe15479590203c7189f37dff5c
Petya Yakimova <petya_yakimova@abv.bg>
2026-06-28 19:36:21 +0300

    Add notepad test

--- NotepadDesktopTests/Tests/NotepadTests.cs
+++ NotepadDesktopTests/Tests/NotepadTests.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlaUI.Core.AutomationElements;
+using NUnit.Framework;
+using NotepadTests.Base;
 
-namespace NotepadDesktopTests.Tests
+namespace NotepadTests.Tests;
+
+public class NotepadTests : TestBase
 {
-    internal class NotepadTests
+    [Test]
+    public void Notepad_Should_Open()
     {
+        var window = App.GetMainWindow(Automation);
+
+        Assert.That(window, Is.Not.Null);
+        Assert.That(window.Title, Does.Contain("Notepad"));
     }
 }
