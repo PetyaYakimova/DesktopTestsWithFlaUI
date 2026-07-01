e83a0d25be1cb0db32534216b2da7eb4195c10ee
Petya Yakimova <petya_yakimova@abv.bg>
2026-07-01 09:58:04 +0300

    Add wait helper

---
+++ NotepadDesktopTests/Helpers/WaitHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+public static class WaitHelper
+{
+    public static void WaitUntil(
+        Func<bool> condition,
+        int timeoutSeconds = 10)
+    {
+        var sw = Stopwatch.StartNew();
+
+        while (sw.Elapsed.TotalSeconds < timeoutSeconds)
+        {
+            if (condition())
+                return;
+
+            Thread.Sleep(200);
+        }
+
+        throw new TimeoutException();
+    }
+}
